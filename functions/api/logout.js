a5ac9213aee8d929c1f73ca7283fee88b048a69c
orlandmanagement-alt <orlandmanagement@gmail.com>
2026-03-06 03:36:05 +0700

    Update logout.js
    Signed-off-by: orlandmanagement-alt <orlandmanagement@gmail.com>

--- functions/api/logout.js
+++ functions/api/logout.js
@@ -1,10 +1,10 @@
-import { json, readCookie, cookie } from "../_lib.js";
+// App/functions/api/logout.js
+import { json } from "../_lib.js";
 
-export async function onRequestPost({ request, env }) {
-  const sid = readCookie(request, "sid");
-  if (sid) await env.KV.delete(`sess:${sid}`);
-
-  const res = json(200, "ok", { logged_out: true });
-  res.headers.append("set-cookie", cookie("sid", "", { maxAge: 0 }));
-  return res;
+export async function onRequestPost({ env, data }) {
+  const sid = data.session?.sid;
+  if (sid) {
+    try { await env.KV.delete(`sess:${sid}`); } catch {}
+  }
+  return json(200, "ok", { logged_out: true });
 }
