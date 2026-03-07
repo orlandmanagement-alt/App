2647101a2bbb8421d7cd4dd15d10251bbe8f805a
orlandmanagement-alt <orlandmanagement@gmail.com>
2026-03-07 19:06:07 +0700

    Delete functions directory
    Signed-off-by: orlandmanagement-alt <orlandmanagement@gmail.com>

--- functions/api/logout.js
+++
@@ -1,9 +0,0 @@
-import { json } from "../_lib.js";
-
-export async function onRequestPost({ env, data }) {
-  const sid = data.session?.sid;
-  if (sid) { try { await env.KV.delete(`sess:${sid}`); } catch {} }
-  const res = json(200,"ok",{ logged_out:true });
-  res.headers.append("set-cookie", "sid=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0");
-  return res;
-}
